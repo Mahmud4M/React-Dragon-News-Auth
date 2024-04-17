3dc86b50f9004d9d2e4483647da047b65c455054
Mahmud4M <mahmud57555@gmail.com>
2024-04-17 23:22:50 +0600

    Firbase secreet

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDY1dizNUHItcMMb9k3GemcIRcoD18SlOs",
-  authDomain: "react-dragon-news-auth-d1309.firebaseapp.com",
-  projectId: "react-dragon-news-auth-d1309",
-  storageBucket: "react-dragon-news-auth-d1309.appspot.com",
-  messagingSenderId: "900249833406",
-  appId: "1:900249833406:web:13cb0eba52531a3cdeb3ef"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
--- src/Pages/LatestNews.jsx
+++ src/Pages/LatestNews.jsx
@@ -7,7 +7,7 @@ const LatestNews = () => {
             <div className="flex items-center">
                 <button className="btn bg-orange-400 mr-3">Secondary</button>
                 <div>
-                    <Marquee pauseOnHover={true} speed={30}>
+                    <Marquee pauseOnHover={true} speed={50}>
                         <Link className="mr-5 text-black no-underline"><span className="text-red-400">Match Highlights:</span> I can be a React component, multiple React components, or just some text.....</Link>
                         <Link className="mr-5 text-black no-underline"><span className="text-red-400">News Highlights:</span> I can be a React component, multiple React components, or just some text.....</Link>
                         <Link className="mr-5 text-black no-underline"><span className="text-red-400">Road Highlights:</span> I can be a React component, multiple React components, or just some text.....</Link>
